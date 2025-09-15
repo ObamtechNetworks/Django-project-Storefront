551e133f4c908f5bcbc28b8ca3bd15d0f1af26cc
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2025-09-15 22:08:14 +0100

    Learnt about Model Serializer, a better to create serializers for django models
    learnt about how we can specify fields we want to expose to the outside

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -9,19 +9,19 @@ from rest_framework import serializers
 from store.models import Product, Collection
 
 # through this we can include nested serializer object in the product serializer
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+# class CollectionSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
 
-class ProductSerializer(serializers.Serializer):
+# class ProductSerializer(serializers.Serializer):
     # Define the fields that you want to serialize
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price') # source is used to map model field name to serializer field name, here we are renaming unit_price to price
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price') # source is used to map model field name to serializer field name, here we are renaming unit_price to price
     
     # creating custom serialized field that is not present in the model
     # this field will be read-only, we cannot use it to update or create a product
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     
     # Serializing related objects ---------------------------------- using primary key related field
     # collection = serializers.PrimaryKeyRelatedField(
@@ -40,13 +40,48 @@ class ProductSerializer(serializers.Serializer):
     
     # -------------------------------------------------- using HyperlinkedRelatedField, dependencies: context in view and url pattern name
     # another way to serilize related object is to use hyperlinked related field
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection-detail' # this should be the name of the url pattern for the
+    # ) # we would need to pass the request context in the Product 'view' while initializing the serializer for this to work
+    
+    
+    # # this method is used by the SerializerMethodField to get the value for the field
+    # # we converted the float to Decimal for consistency with unit_price field type
+    # def calculate_tax(self,product: Product):
+    #     return product.unit_price * Decimal(1.1)
+    
+# LET'S LEARN ABOUT MODEL SERIALIZER WHICH IS A MORE CONCISE WAY TO CREATE SERIALIZERS FOR DJANGO MODELS
+# A ModelSerializer is a type of serializer that is tied to a specific Django model.
+class ProductSerializer(serializers.ModelSerializer):
+    # we can override the model field attributes here if needed
+    # price = serializers.DecimalField(
+    #     max_digits=6, decimal_places=2, source='unit_price'
+    # )  # renaming unit_price to price
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     collection = serializers.HyperlinkedRelatedField(
         queryset=Collection.objects.all(),
-        view_name='collection-detail' # this should be the name of the url pattern for the
-    ) # we would need to pass the request context in the Product 'view' while initializing the serializer for this to work
-    
-    
-    # this method is used by the SerializerMethodField to get the value for the field
-    # we converted the float to Decimal for consistency with unit_price field type
-    def calculate_tax(self,product: Product):
+        view_name='collection-detail'
+    )  # using hyperlinked related field for collection, by default model serializer would use PrimaryKeyRelatedField
+
+    class Meta:
+        model = Product  # specify the model to be serialized
+        fields = [
+            'id',
+            'title',
+            'unit_price',
+            'price_with_tax',
+            'collection',
+        ]  # specify the fields to be included in the serialization
+        # note the order matters here, it will be the order in the serialized output
+        # we can do __all__ to include all fields, but it's not recommended for production code
+        # alternatively we can use exclude = ['description'] to exclude specific fields
+
+    def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+    
+# we can also create a serializer for the Collection model if needed
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', ]
