8f255f77d91218869b7c2ad8200b981bd1c1dc64
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2025-09-20 11:58:13 +0100

    added a basename to the products urls, since we have overriden the ProductViewset

--- storefront2/store/urls.py
+++ storefront2/store/urls.py
@@ -8,7 +8,7 @@ from . import views
 router = routers.DefaultRouter()  # creates a router instance
 
 # register the viewsets with the router
-router.register(r'products', views.ProductViewSet)
+router.register(r'products', views.ProductViewSet, basename='products')
 router.register(r'collections', views.CollectionViewSet)
 # next is to include the router urls in the urlpatterns
 # pprint(router.urls)
