3d2291e66278a41563beba3b8702b50daa347ff0
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2025-09-26 03:02:57 +0100

    added validators to quantity field for CartItem

--- storefront2/store/models.py
+++ storefront2/store/models.py
@@ -116,7 +116,10 @@ class Cart(models.Model):
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+    
     
     class Meta:
         # Ensure that each product can only appear once per cart
