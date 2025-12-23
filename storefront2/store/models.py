172019147be6a1d583995ec2c885325f458d4675
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2025-12-23 13:30:53 +0100

    create custom permissions for the Order class, using Meta class and list of permission tuples

--- storefront2/store/models.py
+++ storefront2/store/models.py
@@ -91,6 +91,12 @@ class Order(models.Model):
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+    # Custom permission to allow order cancellation
+    class Meta:
+        permissions = [
+            ('cancel_order', 'Can cancel order'),
+        ]
 
 
 class OrderItem(models.Model):
