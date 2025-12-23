7f21f47229640df0fadcd468de265b54fe095b33
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2025-12-23 13:19:44 +0100

    referehing about the extending the user model through the AbstractUser model

--- storefront2/core/admin.py
+++ storefront2/core/admin.py
@@ -1,4 +1,4 @@
-from core.models import User
+from .models import User
 from store.models import Product
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
