8c63aee77611c651462a43ea3c7418a32f81c24f
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2025-12-25 03:31:51 +0100

    updated the Customer model to include  a user_id field
    ran migrations for this model update

--- storefront2/store/models.py
+++ storefront2/store/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
@@ -69,6 +70,7 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True, blank=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
