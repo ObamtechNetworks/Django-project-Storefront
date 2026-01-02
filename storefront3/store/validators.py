7e675bd8061b086a06dbd5c78a6ae994d48b2842
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2026-01-02 14:17:47 +0100

    added validation to image uploads
    created a validation module with file size specification
    
    using the django module django.forms import ValidationError & django.core.validators import MinValueValidtor

---
+++ storefront3/store/validators.py
@@ -0,0 +1,8 @@
+
+from django.forms import ValidationError
+
+def validate_file_size(file):
+    max_size_kb = 50 # 50 KB
+    
+    if file.size > max_size_kb * 1024:
+        raise ValidationError(f'Maximum file size is {max_size_kb}KB!')
