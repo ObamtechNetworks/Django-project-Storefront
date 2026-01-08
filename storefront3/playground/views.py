05d66e0de0fc06cc5b577f6e763735766566c0ac
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2026-01-08 22:00:56 +0100

    installed django email template package
    created an email template folder
    
    used the templated mail package to send email using BaseEmailMessage class

--- storefront3/playground/views.py
+++ storefront3/playground/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
 from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
     try:
         # send_mail('subject', 'message', 'webmaster@localhost', ['user@example.com'])
         # mail_admins('subject', 'Here is the message.', html_message='<b>This is bold</b>')
-        message = EmailMessage(
-            'Subject here',
-            'Body goes here',
-            'webmaster@localhost',
-            ['user@example.com']
-        )
+        # message = EmailMessage(
+        #     'Subject here',
+        #     'Body goes here',
+        #     'webmaster@localhost',
+        #     ['user@example.com']
+        # )
         
-        message.attach_file('playground/static/images/dog.png')
-        message.send()
+        # message.attach_file('playground/static/images/dog.png')
+        # message.send()
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Bamidele Michael'}
+        )
+        message.send(to=['obams@example.com'])
     except BadHeaderError:
         pass
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    return render(request, 'hello.html', {'name': 'Bamidele'})
