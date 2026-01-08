6c50d861a7c417c01913fff102d08c762652c9bb
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2026-01-08 21:45:47 +0100

    sending emails using Django send_mail module and mail_admins

--- storefront3/playground/views.py
+++ storefront3/playground/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 
 
 def say_hello(request):
+    try:
+        # send_mail('subject', 'message', 'webmaster@localhost', ['user@example.com'])
+        mail_admins('subject', 'Here is the message.', html_message='<b>This is bold</b>')
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
