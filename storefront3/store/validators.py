a8579f13432961a88a77833d4acdf71c3c419ee2
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2026-01-08 20:46:20 +0100

    increased image max size to 2MB
    configured frontend client,installed djanog-cors-headers package
    
    configured the package as well

--- storefront3/store/validators.py
+++ storefront3/store/validators.py
@@ -2,7 +2,7 @@
 from django.forms import ValidationError
 
 def validate_file_size(file):
-    max_size_kb = 50 # 50 KB
+    max_size_kb = 2000 # 2000 KB
     
     if file.size > max_size_kb * 1024:
         raise ValidationError(f'Maximum file size is {max_size_kb}KB!')
--- storefront3/storefront/settings.py
+++ storefront3/storefront/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'corsheaders',
     'rest_framework',
     'djoser',
     'playground',
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -69,6 +71,11 @@ INTERNAL_IPS = [
     # ...
     '127.0.0.1',
     # ...
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8001',
+    'http://127.0.0.1:8001',
 ]
 
 ROOT_URLCONF = 'storefront.urls'
