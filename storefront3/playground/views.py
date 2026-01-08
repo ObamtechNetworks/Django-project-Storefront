fbfc06affa1a0773074b674e6f2b69e263b3a91a
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2026-01-08 21:50:22 +0100

    attaching images or files using the EmailMessage class

--- storefront3/playground/views.py
+++ storefront3/playground/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 
 
 def say_hello(request):
     try:
         # send_mail('subject', 'message', 'webmaster@localhost', ['user@example.com'])
-        mail_admins('subject', 'Here is the message.', html_message='<b>This is bold</b>')
+        # mail_admins('subject', 'Here is the message.', html_message='<b>This is bold</b>')
+        message = EmailMessage(
+            'Subject here',
+            'Body goes here',
+            'webmaster@localhost',
+            ['user@example.com']
+        )
+        
+        message.attach_file('playground/static/images/dog.png')
+        message.send()
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
