27c322dd786ea12d2129748a1baf880b4fba0beb
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2026-01-29 21:04:44 +0100

    Configure production database for Render

--- storefront3/storefront/settings/prod.py
+++ storefront3/storefront/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .common import *
 
 DEBUG = False
@@ -6,3 +7,12 @@ DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['drbamsstorefront.onrender.com']
+
+# Database (Render Postgres)
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
